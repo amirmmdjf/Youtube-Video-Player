2fc091921a574885bbba2363bcebdfeefa76d508
Amirmohammad <amirmmdjf2006@gmail.com>
2024-12-16 11:35:34 +0330

    add type for some variable

--- src/app/components/videoPlayer/VideoPlayer.tsx
+++ src/app/components/videoPlayer/VideoPlayer.tsx
@@ -1,55 +1,53 @@
 'use client';
 
-import { useState, useRef, useEffect } from "react";
-import YouTube from 'react-youtube';
+import { useState, useRef, useEffect, RefObject } from "react";
+import YouTube, { YouTubeProps } from 'react-youtube';
 
-const VideoPlayer = () => {
-  const [urlVideo, setUrlVideo] = useState('https://www.youtube.com/watch?v=PLLRRXURicM');
-  const [videoId, setVideoId] = useState(() => {
+const VideoPlayer: React.FC = () => {
+  const [urlVideo, setUrlVideo] = useState<string>('https://www.youtube.com/watch?v=PLLRRXURicM');
+  const [videoId, setVideoId] = useState<string>(() => {
     const match = urlVideo.match(/(?:youtube\.com.*(?:\?|&)v=|youtu\.be\/)([^&]+)/);
     return match ? match[1] : '';
   });
-  const [err, setErr] = useState(false);
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+  const [err, setErr] = useState<boolean>(false);
+  const [screenWidth, setScreenWidth] = useState<number>(window.innerWidth);
 
   useEffect(() => {
-    const handleResize = () => setScreenWidth(window.innerWidth);
+    const handleResize = (): void => setScreenWidth(window.innerWidth);
 
     window.addEventListener('resize', handleResize);
 
-    console.log(screenWidth)
+    console.log(screenWidth);
     return () => {
       window.removeEventListener('resize', handleResize);
     };
-    
   }, [screenWidth]);
 
-  const inputRef = useRef(null);
+  const inputRef: RefObject<HTMLInputElement> = useRef(null);
 
-  const extractVideoId = (url:string) => {
+  const extractVideoId = (url: string): string | null => {
     const match = url.match(/(?:youtube\.com.*(?:\?|&)v=|youtu\.be\/)([^&]+)/);
     return match ? match[1] : null;
   };
 
-  const onPlayerReady = () => {
-    console.log('Player is ready');
+  const onPlayerReady = (event: YouTubeProps['onReady']) => {
+    console.log('Player is ready', event.target);
   };
 
-  const onPlayerError = (event) => {
+  const onPlayerError = (event: { data: number }): void => {
     console.error('Error:', event.data);
   };
 
-
-  const opts = {
-    height: screenWidth > 750 ? '400' : '350' ,
+  const opts: YouTubeProps['opts'] = {
+    height: screenWidth > 750 ? '400' : '350',
     width: screenWidth > 750 ? '700' : '300',
     playerVars: {
-      autoplay: 0, 
+      autoplay: 0,
     },
   };
 
-  const setYoutubeVideoIdHandler = () => {
-    const newUrl = inputRef.current.value;
+  const setYoutubeVideoIdHandler = (): void => {
+    const newUrl = inputRef.current?.value || '';
 
     if (newUrl.trim().length === 0) {
       setErr(true);
@@ -69,11 +67,8 @@ const VideoPlayer = () => {
 
   return (
     <>
-      <div className="flex flex-col max-sm:pt-10  mb-5">
-
-      <h1 className="mt-10 text-[40px] text-[#e93434]">Youtube Video player</h1>
-
-        
+      <div className="flex flex-col max-sm:pt-10 mb-5">
+        <h1 className="mt-10 text-[40px] text-[#e93434]">Youtube Video player</h1>
         <div className="flex justify-center mt-10">
           <input
             className="border-b py-1 px-2 outline-none w-full"
@@ -92,18 +87,17 @@ const VideoPlayer = () => {
       </div>
 
       <div className="mt-8">
-      {videoId ? (
-        <YouTube
-          videoId={videoId}
-          opts={opts}
-          onReady={onPlayerReady}
-          onError={onPlayerError}
-        />
-      ) : (
-        <p className="text-red-500 mt-4">Invalid YouTube URL</p>
-      )}
+        {videoId ? (
+          <YouTube
+            videoId={videoId}
+            opts={opts}
+            onReady={onPlayerReady}
+            onError={onPlayerError}
+          />
+        ) : (
+          <p className="text-red-500 mt-4">Invalid YouTube URL</p>
+        )}
       </div>
-
     </>
   );
 };
